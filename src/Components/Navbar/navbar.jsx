23496ab9c9cf7372c96fc815ab88dd4e0be85f7b
Bernard Louie <pacanza.berns4884@gmail.com>
2024-12-16 20:17:03 +0800

    mugana na ang dark mode og light mode

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React from "react";
 import { MdLightMode } from "react-icons/md";
 import { CiLogin } from "react-icons/ci";
 import { FaPenAlt } from "react-icons/fa";
 
 const Navbar = ({ toggleTheme, isLightMode }) => {
   return (
-    <nav className='relative z-10 flex w-full justify-between items-center pt-3 select-none'>
-      <div className='cursor-pointer' onClick={toggleTheme}>
-        <h1 className={`text-2xl text-white ${isLightMode} ${isLightMode ? 'text-[#060610]' : 'text-[#bdbcbe]'}`}><MdLightMode /></h1>
+    <nav className="relative z-10 flex w-full justify-between items-center pt-3 select-none">
+      <div className="cursor-pointer" onClick={toggleTheme}>
+        <h1
+          className={`text-2xl ${isLightMode} ${
+            isLightMode ? "text-[#060610]" : "text-[#bdbcbe]"
+          }`}
+        >
+          <MdLightMode />
+        </h1>
       </div>
-      <div className='flex justify-center items-center'>
-        <h1 className={`text-xl font-normal mr-2 ${isLightMode ? 'text-[#6a39f3]' : 'text-[#584295]'}`}><FaPenAlt /></h1>
-        <h1 className={`text-xl font-light ${isLightMode ? 'text-[#060610]' : 'text-[#bdbcbe]'}`}>Absence Counter</h1>
+      <div className="flex justify-center items-center">
+        <h1
+          className={`text-xl font-normal mr-2 ${
+            isLightMode ? "text-[#6a39f3]" : "text-[#584295]"
+          }`}
+        >
+          <FaPenAlt />
+        </h1>
+        <h1
+          className={`text-xl font-light ${
+            isLightMode ? "text-[#060610]" : "text-[#bdbcbe]"
+          }`}
+        >
+          Absence Counter
+        </h1>
       </div>
-      <div className='cursor-pointer'>
-        <h1 className={`text-2xl ${isLightMode ? 'text-[#060610]' : 'text-[#bdbcbe]'}`}><CiLogin /></h1>
+      <div className="cursor-pointer">
+        <h1
+          className={`text-2xl ${
+            isLightMode ? "text-[#060610]" : "text-[#bdbcbe]"
+          }`}
+        >
+          <CiLogin />
+        </h1>
       </div>
     </nav>
   );
